483acb336c362ccc97293596b80d34d15eb3c168
Sayaka Vincent <sfv2010@hotmail.fr>
2022-06-01 15:14:19 +0200

    add option and localStrage

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,12 @@
 //---Faire le lien entre un produit de la page d'acceuil et la page produit---
-
+//---Récupération de la châne de requête dans l'url---
 const urlSearchParams = new URLSearchParams(location.search);
 const kanapId = urlSearchParams.get("id");
-// console.log(kanapId);
+ console.log(kanapId); //aprés html?id=
 
-//---Récupérer l'id du produit à afficher avec la déclaration du tableau---
+//---Récupérer l'id du produit à afficher ---
 
-let kanapData = [];
+let kanapData;
 
 const recupererProduitParId = async () => {
   try {
@@ -20,10 +20,12 @@ const recupererProduitParId = async () => {
     .innerText = " Erreur d'affichage - nous sommes désolés ";
   }
 };
-// recupererProduit();
+// recupererProduitParId();
 
-//---Insérer un produit et ses d'tails dans la page Produit---
-const afficherProduit = async () => {
+//---Insérer un produit et ses détails dans la page Produit---
+
+const afficherProduitParId = async () => {
+
     await recupererProduitParId();
 
     document
@@ -43,11 +45,60 @@ const afficherProduit = async () => {
     .getElementById("description")
     .innerText = kanapData.description;
 
-
-    document.querySelector("#colors")
+    document
+    .querySelector("#colors")
     .insertAdjacentHTML("beforeend", kanapData.colors.map((colors) =>
-    `<option value="${colors}">${colors}</option>`))
-    .join("");
+    `<option value="${colors}">${colors}</option>`));
    };
 
-afficherProduit();
+afficherProduitParId();
+
+// ---Ajouter des produits dans le panier---
+// ---Récupération des données sélectionnées par l'utilisateur---
+
+document
+.getElementById("addToCart")
+.addEventListener("click",(event) =>{
+    event.preventDefault();
+    //event.stopPropagation();
+
+    const selectionUser = {
+        id: kanapId,
+        color: document.getElementById('colors').value,
+        quantity: document.getElementById('quantity').value
+    }
+    
+    //---si les options ne sont pas bien sélectionnés---
+    if ((selectionUser.color.length) === 0 && (selectionUser.quantity) <= 0 || (selectionUser.quantity) > 100) {
+        alert ("Veuillez choisir une couleur et une quantité");
+        } else if ((selectionUser.color.length) === 0){
+            alert ("Veuillez choisir une couleur");
+        } else if ((selectionUser.quantity) <= 0 || (selectionUser.quantity) > 100 ) {
+            alert ("Veuillez choisir une quantité entre 1 et 100");
+        };
+    
+    //---confirlation d'ajouter au panier
+
+
+    //---Local strage---
+    let kanapLocalstrage = JSON.parse(localStorage.getItem("produit"));
+
+    //---s'il y a dejà des produits d'enregistré dans le local storage---
+    if(kanapLocalstrage){
+        kanapLocalstrage.push(selectionUser);
+        localStorage.setItem("produit", JSON.stringify(kanapLocalstrage));
+        console.log(kanapLocalstrage);
+    }
+
+    //---s'il n'y a pas de produit d'enregistré dans le local storage---
+    else{
+        kanapLocalstrage = [];
+        kanapLocalstrage.push(selectionUser);
+        localStorage.setItem("produit", JSON.stringify(kanapLocalstrage));
+        console.log(kanapLocalstrage);
+    }
+
+
+});
+
+
