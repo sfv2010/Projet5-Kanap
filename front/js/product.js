616afae21e20da4b51434d00b10b5019ead10446
Sayaka Vincent <sfv2010@hotmail.fr>
2022-06-03 12:02:54 +0200

    add finf method

--- front/js/product.js
+++ front/js/product.js
@@ -60,44 +60,59 @@ document
 .getElementById("addToCart")
 .addEventListener("click",(event) =>{
     event.preventDefault();
-    //event.stopPropagation();
-
     const selectionUser = {
+        name:kanapData.name,
         id: kanapId,
         color: document.getElementById('colors').value,
-        quantity: document.getElementById('quantity').value
+        quantity: parseInt( document.getElementById('quantity').value)
     }
+    // console.log(selectionUser);
     
     //---si les options ne sont pas bien sélectionnés---
     if ((selectionUser.color.length) === 0 && (selectionUser.quantity) <= 0 || (selectionUser.quantity) > 100) {
         alert ("Veuillez choisir une couleur et une quantité");
+        return;
         } else if ((selectionUser.color.length) === 0){
             alert ("Veuillez choisir une couleur");
+            return;
         } else if ((selectionUser.quantity) <= 0 || (selectionUser.quantity) > 100 ) {
-            alert ("Veuillez choisir une quantité entre 1 et 100");
+            alert ("Veuillez choisir une quantité entre 1 et 100");   
+            return;
+        } else { //---confirlation d'ajouter au panier---  
+            alert (`Merci, vous avez ajouté ${selectionUser.quantity} ${selectionUser.name} ${selectionUser.color}  à votre panier ! `)
+            location.href= "cart.html" ;
         };
     
-    //---confirlation d'ajouter au panier
-
-
     //---Local strage---
-    let kanapLocalstrage = JSON.parse(localStorage.getItem("produit"));
+    //---récupérer les keys et les values qui sont dans le local strage en convertissant aux objets Javascript---
+    let kanapLocalstrage = JSON.parse(localStorage.getItem("kanapProduit"));
+    
+    //---function pour ajouter un produit dans le local strage---
+    const ajouterLocalstrage = () => {
+        kanapLocalstrage.push(selectionUser);     
+    };
+    const stockerLocalstrage = () =>{
+        localStorage.setItem("kanapProduit", JSON.stringify(kanapLocalstrage));//stocker la key "kanapProduit" et les values en convertissant au format Json
+    };
 
     //---s'il y a dejà des produits d'enregistré dans le local storage---
     if(kanapLocalstrage){
-        kanapLocalstrage.push(selectionUser);
-        localStorage.setItem("produit", JSON.stringify(kanapLocalstrage));
-        console.log(kanapLocalstrage);
+        //---si le produit identique est déja présent dans le panier ( même id + même couleur),on incrémente---
+        let memeProduit = kanapLocalstrage.find( (produit) => produit.id === selectionUser.id && produit.color === selectionUser.color);
+        if (memeProduit != undefined ){
+            memeProduit.quantity = parseInt(selectionUser.quantity += memeProduit.quantity);
+        }else {
+            ajouterLocalstrage();     
+        }
+        stockerLocalstrage();  
     }
 
     //---s'il n'y a pas de produit d'enregistré dans le local storage---
     else{
         kanapLocalstrage = [];
-        kanapLocalstrage.push(selectionUser);
-        localStorage.setItem("produit", JSON.stringify(kanapLocalstrage));
-        console.log(kanapLocalstrage);
-    }
-
+        ajouterLocalstrage();  
+        stockerLocalstrage();  
+    };
 
 });
 
