a70c9cdc305801c862bda0d2f2d33d29c2b794c5
Sayaka Vincent <sfv2010@hotmail.fr>
2022-06-11 23:57:46 +0200

    add html tag

--- front/js/script.js
+++ front/js/script.js
@@ -1,75 +1,90 @@
 // ---Récupérer les donnée par API ---
 
-let kanapData;
-
-const callApi = async() => {
-    try{
-      const res = await fetch("http://localhost:3000/api/products");
-      kanapData = await res.json();
+const callApi = async () => {
+    try {
+      const products = await fetch("http://localhost:3000/api/products");
+      return products.json();
     }
     catch(err){
       document
       .getElementById("items")
       .innerText = " Erreur d'affichage - nous sommes désolés ";
     }
-  }
-callApi();
-
-//---Insérer les produits dans la page d'acceuil (Affichage les 8 photos et les textes)---
-
-const displayProducts = async() => {
-    await callApi();
-
-    document
-    .getElementById("items")
-    .innerHTML = kanapData.map((value) =>
-    
-      `<a href="./product.html?id=${value._id}">
-          <article>
-            <img src="${value.imageUrl}" alt="${value.altTxt}">
-            <h3 class="productName">${value.name}</h3>
-            <p class="productDescription">${value.description}</p>
-          </article>
-        </a>`
-    )
-  .join("");
 };
+
+ //---Insérer les produits dans la page d'acceuil (Affichage les 8 photos et les textes)---
+ 
+const displayProducts = async () => {
+    const result = await callApi();
+    //console.log(result);
+    for (let i = 0; i < result.length; i++) {
+        //---Créer des nouveaux éléments---
+        //---<a href>---
+        let productA = document.createElement("a");
+        document.getElementById("items").appendChild(productA);
+        productA.href = `./product.html?id=${result[i]._id}`;
+
+        //---<article>---
+        let productArticle = document.createElement("article");
+        productA.appendChild(productArticle);
+
+        //---<img src alt>---
+        let productImg = document.createElement("img");
+        productArticle.appendChild(productImg);
+        productImg.src = result[i].imageUrl;
+        productImg.alt = result[i].altTxt;
+
+        //---<h3 class = "productName">---
+        let productH3 = document.createElement("h3");
+        productArticle.appendChild(productH3);
+        productH3.classList.add("productName");
+        productH3.innerText = result[i].name;
+
+        //---<p class = "productDescription">---
+        let productP = document.createElement("p");
+        productArticle.appendChild(productP);
+        productP.classList.add("productDescription");
+        productP.innerText = result[i].description;
+    } 
+};
+
 displayProducts();
 
 
-// const endPoint = "http://localhost:3000/api/products";
 
-// const recupererProduit = async url => {
-//   try {
-//     const produit = await fetch(url);
-//     return produit.json();
+// ---Récupérer les donnée par API ---
+//let kanapData;
+
+// const callApi = async() => {
+//     try{
+//       const res = await fetch("http://localhost:3000/api/products");
+//       kanapData = await res.json();
+//     }
+//     catch(err){
+//       document
+//       .getElementById("items")
+//       .innerText = " Erreur d'affichage - nous sommes désolés ";
+//     }
 //   }
-//   catch(err){
+// callApi();
+
+// //---Insérer les produits dans la page d'acceuil (Affichage les 8 photos et les textes)---
+
+// const displayProducts = async() => {
+//     await callApi();
+
 //     document
 //     .getElementById("items")
-//     .innerText = " Erreur d'affichage - nous sommes désolés ";
-//   }
+//     .innerHTML = kanapData.map(value =>
+    
+//       `<a href="./product.html?id=${value._id}">
+//           <article>
+//             <img src="${value.imageUrl}" alt="${value.altTxt}">
+//             <h3 class="productName">${value.name}</h3>
+//             <p class="productDescription">${value.description}</p>
+//           </article>
+//         </a>`
+//     )
+//   .join("");
 // };
-
-// const afficherProduit = async () => {
-//   const result = await recupererProduit(endPoint);
-//   console.log(result);
-//   let baliseA = "";
-//   const items = document.getElementById("items");
-
-//   for (let i = 0; i < result.length; i++) {
-//     baliseA = `${baliseA} <a href="./product.html?id=${result[i]._id}">
-//         <article>
-//           <img src="${result[i].imageUrl}" alt="${result[i].altTxt}">
-//           <h3 class="productName">${result[i].name}</h3>
-//           <p class="productDescription">${result[i].description}</p>
-//         </article>
-//       </a>`;
-//   }
-
-//   items.innerHTML = baliseA;
-  
-// };
-
-// afficherProduit();
-
+// displayProducts();
